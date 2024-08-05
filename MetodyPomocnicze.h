5f44d208b6bb59d87e07ad9f894cc9a897fd2cc0
Dominika Osielska <dominika.osielska.programistka@gmail.com>
2024-08-05 19:56:27 +0200

    Divide KsiazkaAdresowa to smaller classes

---
+++ MetodyPomocnicze.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class MetodyPomocnicze
+{
+public:
+    static string konwerjsaIntNaString(int liczba);
+};
