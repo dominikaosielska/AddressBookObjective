fd60ea8abef4baa43aa655161676133170b71eb4
Dominika Osielska <dominika.osielska.programistka@gmail.com>
2024-08-12 20:28:58 +0200

    Load user at start

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -7,7 +7,9 @@ using namespace std;
 class KsiazkaAdresowa
 {
     UzytkownikMenadzer uzytkownikMenadzer;
+
 public:
+    KsiazkaAdresowa();
     void rejestracjaUzytkownika();
     void wypiszWszystkichUzytkownikow();
 };
